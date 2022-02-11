600f8ab8f629e3f7e7654af7a4112854c6be14ab
tuoluoleaf <443383008@qq.com>
2022-02-11 14:05:24 +0800

    README.md

--- chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -1,15 +1,14 @@
 package org.smart4j.chapter1;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.FutureTask;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
@@ -19,7 +18,6 @@ public class HelloServlet extends HttpServlet {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String currentTime = dateFormat.format(new Date());
         req.setAttribute("currentTime", currentTime);
-//        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
-        resp.setStatus(404);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
     }
 }
